2609fc4956eaf11c8c14fb8f97c4fa7c0dbeca80
Peter Shevchuk <raf02041994@gmail.com>
2020-10-11 22:34:57 +0300

    run build

---
+++ build/precache-manifest.eb57f8f625f16701d671201ef29a72ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "385ac266e27d3ddd047e05792e22f28f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9c94ec40f38841aeaff",
+    "url": "/static/css/main.e138b57a.chunk.css"
+  },
+  {
+    "revision": "3631f38a4f6fa5d21435",
+    "url": "/static/js/2.69224cce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.69224cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9c94ec40f38841aeaff",
+    "url": "/static/js/main.88d2665a.chunk.js"
+  },
+  {
+    "revision": "c6d6a987643aa5648a18",
+    "url": "/static/js/runtime-main.6af9711b.js"
+  }
+]);
